ed29939b02e77926bc0aeb131980a5b316afb2dd
hanu607 <jrsky723@gmail.com>
2024-03-17 21:03:11 +0900

    Main Navigation FIx

--- lib/common/main_navigation_screen.dart
+++ lib/common/main_navigation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:moodiary/features/calendar/calendar_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   final String tab;
@@ -15,10 +16,11 @@ class MainNavigationScreen extends StatefulWidget {
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   final List<String> _tabs = [
-    'home',
-    'diary',
     'calendar',
-    'profile',
+    'dashboard',
+    'xxx',
+    'store',
+    'settings',
   ];
 
   late int _selectedIndex = _tabs.indexOf(widget.tab);
@@ -30,26 +32,30 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         children: [
           Offstage(
             offstage: _selectedIndex != 0,
-            child: const Center(
-              child: Text("Home"),
-            ),
+            child: const CalendarScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 1,
             child: const Center(
-              child: Text("Diary"),
+              child: Text("Dashboard"),
             ),
           ),
           Offstage(
             offstage: _selectedIndex != 2,
             child: const Center(
-              child: Text("Calendar"),
+              child: Text('Progress'),
             ),
           ),
           Offstage(
             offstage: _selectedIndex != 3,
             child: const Center(
-              child: Text("Profile"),
+              child: Text("Store"),
+            ),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: const Center(
+              child: Text("Settings"),
             ),
           ),
         ],
@@ -57,23 +63,24 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       bottomNavigationBar: NavigationBar(
         destinations: const [
           NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.house),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.penToSquare),
-            selectedIcon: FaIcon(FontAwesomeIcons.solidPenToSquare),
-            label: 'Diary',
-          ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.calendarCheck),
-            selectedIcon: FaIcon(FontAwesomeIcons.solidCalendarCheck),
+            icon: FaIcon(FontAwesomeIcons.calendarDay),
             label: 'Calendar',
           ),
           NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.circleUser),
-            selectedIcon: FaIcon(FontAwesomeIcons.solidCircleUser),
-            label: 'Profile',
+            icon: FaIcon(FontAwesomeIcons.chartSimple),
+            label: 'Dashboard',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.circlePlus),
+            label: 'Add Diary',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.store),
+            label: 'Store',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.gear),
+            label: 'Settings',
           ),
         ],
         selectedIndex: _selectedIndex,
