37ae54e4685a773a6ae935c90d63270fc9afebbe
ms <alstjq1012@gmail.com>
2024-03-27 22:34:38 +0900

    info_tab's issues fix
    1. veritcal divider 안나오는것 sizedBox로 height설정해서 수정
    2. infoButton widget을 padding까지넣어서 리펙토링함
    3. calendar 좌우 이동으로 월 이동할 때 해당 일로 이동되는것 오류 발생해서 1일로 다시 바꿈

--- lib/features/calendar/widgets/info_tab.dart
+++ lib/features/calendar/widgets/info_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:moodiary/constants/gaps.dart';
 import 'package:moodiary/constants/sizes.dart';
 
 class InfoTab extends StatefulWidget {
@@ -17,58 +18,31 @@ class InfoTab extends StatefulWidget {
 }
 
 class _InfoTapState extends State<InfoTab> {
-  bool isReverse = false;
-
-  void _reverse() {
-    setState(() {
-      isReverse = !isReverse;
-    });
-    print(isReverse);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        const Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size14,
+          ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: const infoIconButton(
-                    icon: FontAwesomeIcons.shareNodes,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: const infoIconButton(
-                    icon: FontAwesomeIcons.penToSquare,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: const infoIconButton(
-                    icon: FontAwesomeIcons.trashCan,
-                  ),
-                ),
-              ),
+              pInfoButton(icon: FontAwesomeIcons.shareNodes),
+              pInfoButton(icon: FontAwesomeIcons.penToSquare),
+              pInfoButton(icon: FontAwesomeIcons.trashCan),
             ],
           ),
         ),
         Container(
-          margin: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(
+            Sizes.size10,
+          ),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(
+              Sizes.size10,
+            ),
             color: Colors.grey.shade100,
             boxShadow: [
               BoxShadow(
@@ -78,59 +52,71 @@ class _InfoTapState extends State<InfoTab> {
               )
             ],
           ),
-          child: GestureDetector(
-            onTap: _reverse,
-            child: Row(
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(Sizes.size14),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: Sizes.size18,
-                          backgroundImage: widget.image,
+          child: Row(
+            children: [
+              Flexible(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(Sizes.size14),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: Sizes.size18,
+                        backgroundImage: widget.image,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: Sizes.size7,
                         ),
-                        Container(
-                          margin: const EdgeInsets.only(top: Sizes.size7),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: Sizes.size10,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: Sizes.size10,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            Sizes.size5,
                           ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(Sizes.size5),
-                            color: Colors.grey.shade300,
+                          color: Colors.grey.shade300,
+                        ),
+                        child: Text(
+                          widget.date > 9
+                              ? '${widget.date} 일'
+                              : '0${widget.date} 일',
+                          style: TextStyle(
+                            fontSize: Sizes.size12,
+                            color: Colors.grey.shade700,
                           ),
-                          child: Text(
-                            widget.date > 9
-                                ? '${widget.date} 일'
-                                : '0${widget.date} 일',
-                            style: TextStyle(
-                              fontSize: Sizes.size12,
-                              color: Colors.grey.shade700,
-                            ),
-                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: Sizes.size52,
+                child: VerticalDivider(
+                  color: Colors.black,
+                ),
+              ),
+              Flexible(
+                flex: 7,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: const Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.babyCarriage,
+                        ),
+                        Gaps.h10,
+                        FaIcon(
+                          FontAwesomeIcons.baby,
                         ),
                       ],
                     ),
                   ),
                 ),
-                Flexible(
-                  flex: 7,
-                  child: Container(
-                    child: isReverse
-                        ? const Text(
-                            "testdsadjdasdhskaldhdhasjkhdsjldsdadsadadddadsadadshkjdasdasjkhdskjhkdsajhsadadhaldhakldhsaldkh",
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          )
-                        : const FaIcon(
-                            FontAwesomeIcons.chevronDown,
-                          ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ],
@@ -138,19 +124,27 @@ class _InfoTapState extends State<InfoTab> {
   }
 }
 
-class infoIconButton extends StatelessWidget {
+class pInfoButton extends StatelessWidget {
   final IconData icon;
-  const infoIconButton({
+  const pInfoButton({
     super.key,
     required this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FaIcon(
-      icon,
-      size: Sizes.size14,
-      color: Colors.grey.shade700,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: Sizes.size10,
+      ),
+      child: GestureDetector(
+        onTap: () {},
+        child: FaIcon(
+          icon,
+          size: Sizes.size14,
+          color: Colors.grey.shade700,
+        ),
+      ),
     );
   }
 }
