540945f96f9ea81da4eda5b7f99c30929f156845
hanu607 <jrsky723@gmail.com>
2024-04-03 01:11:18 +0900

    Dashboard - DateSelector
    - DateSelector refactoring
    - constant camelCase로, min/max year 추가
    - DashboardScreen: 월간, 연간 탭, DateSelector onTap 구현

---
+++ lib/common/widgets/date_selector_tab.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:moodiary/constants/sizes.dart';
+
+class DateSelectorTab extends StatelessWidget {
+  final String text;
+  final void Function() onTap;
+
+  const DateSelectorTab({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(Sizes.size8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+            const Icon(Icons.arrow_drop_down),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/constants/date.dart
+++ lib/constants/date.dart
@@ -1,1 +1,4 @@
-const List<String> WEEKDAYS = ['일', '월', '화', '수', '목', '금', '토'];
+const List<String> weekDays = ['일', '월', '화', '수', '목', '금', '토'];
+
+const int minYear = 2000;
+const int maxYear = 2100;
