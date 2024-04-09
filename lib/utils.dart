dc0f26985f67a0c1cdc7251d4379e5f26fda2bbd
hanu607 <jrsky723@gmail.com>
2024-04-10 02:19:44 +0900

    DashBoard - MoodFlowChart
    - MoodFlowChart 구현
      - 날짜 기분에 따라, 그래프 그리기
      - 임시 데이터를 위한 utils function
      - MoodEntry
      - mood Enum
      - FlowChart: legend_painter + line_chart_painter
      - FlowChart 그라데이션 적용
      - date_selector_tab textSize 추가

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,27 @@
+import 'dart:math';
+
+import 'package:moodiary/constants/mood.dart';
+import 'package:moodiary/features/dashboard/models/mood_entry.dart';
+
 bool isSameDay(DateTime date1, DateTime date2) {
   return date1.year == date2.year &&
       date1.month == date2.month &&
       date1.day == date2.day;
+}
+
+Random random = Random();
+
+MoodEntry randomMoodEntry(DateTime date) {
+  return MoodEntry(
+    date: date,
+    mood: Mood.values[random.nextInt(Mood.values.length)],
+  );
+}
+
+List<MoodEntry> generateMoodEntries(DateTime startDate, int days) {
+  List<MoodEntry> entries = [];
+  for (int i = 0; i < days; i++) {
+    entries.add(randomMoodEntry(startDate.add(Duration(days: i))));
+  }
+  return entries;
 }
