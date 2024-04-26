95a4afebdcd696b9d68ff1d28191727bc8f1e835
hanu607 <jrsky723@gmail.com>
2024-04-26 17:04:35 +0900

    DashBoard - Yearly&Monthly Refactoring
    - DashboardScreen을 두개의 dashBoard로 refactoring
      - yearly
      - monthly
    - yearly에서 윤년 고려안한 문제 해결

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moodiary/common/widgets/date_selector_tab.dart';
-import 'package:moodiary/constants/date.dart';
-import 'package:moodiary/constants/gaps.dart';
-import 'package:moodiary/constants/sizes.dart';
-import 'package:moodiary/features/dashboard/widgets/mood_dist_chart.dart';
-import 'package:moodiary/features/dashboard/widgets/mood_flow_chart.dart';
-import 'package:moodiary/features/dashboard/models/mood_entry.dart';
-import 'package:moodiary/features/dashboard/widgets/mood_chart_card.dart';
-import 'package:moodiary/utils.dart';
+import 'package:moodiary/features/dashboard/monthly_dashboard.dart';
+import 'package:moodiary/features/dashboard/yearly_dashboard.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -19,14 +12,6 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen>
     with TickerProviderStateMixin {
   TabController? _tabController;
-  bool _isMonthly = true;
-  final DateTime _now = DateTime.now();
-  int _monthlyYear = DateTime.now().year;
-  int _monthlyMonth = DateTime.now().month;
-  int _yearlyYear = DateTime.now().year;
-  late List<MoodEntry> _monthlyMoodEntries = [];
-  late List<MoodEntry> _yearlyMoodEntries = [];
-  late List<MoodEntry> _modeYearlyMoodEntries = [];
 
   @override
   void initState() {
@@ -36,152 +21,12 @@ class _DashboardScreenState extends State<DashboardScreen>
       vsync: this,
       animationDuration: const Duration(milliseconds: 300),
     );
-
-    _tabController?.addListener(() {
-      setState(() {
-        _isMonthly = _tabController?.index == 0;
-      });
-    });
-    _monthlyMoodEntries = generateMonthlyMoodEntries();
-    _yearlyMoodEntries = generateYearlyMoodEntries();
-    _modeYearlyMoodEntries = modeYearlyMoodEntries(
-      _yearlyMoodEntries,
-      _yearlyYear,
-    );
   }
 
   @override
   void dispose() {
     _tabController?.dispose();
     super.dispose();
-  }
-
-  List<MoodEntry> generateMonthlyMoodEntries() {
-    return generateMoodEntries(
-      DateTime(_monthlyYear, _monthlyMonth),
-      _isMonthly
-          ? daysInMonth(_monthlyYear, _monthlyMonth)
-          : daysInYear(_yearlyYear),
-    );
-  }
-
-  List<MoodEntry> generateYearlyMoodEntries() {
-    return generateMoodEntries(
-      DateTime(_yearlyYear, 1),
-      365,
-    );
-  }
-
-  Future<void> _onDateSelectorTap() async {
-    final ScrollController scrollController = ScrollController();
-
-    final int selectedIndex = _isMonthly
-        ? (_now.year - _monthlyYear) * 12 + _now.month - _monthlyMonth
-        : _now.year - _yearlyYear;
-
-    final int itemCount = _isMonthly
-        ? _now.month + (_now.year - minYear) * 12
-        : _now.year - minYear + 1;
-
-    const double itemExtent = 50;
-
-    final double initialScrollOffset = selectedIndex * itemExtent;
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (scrollController.hasClients) {
-        scrollController.jumpTo(initialScrollOffset);
-      }
-    });
-
-    DateTime? picked = await showModalBottomSheet<DateTime>(
-      clipBehavior: Clip.antiAlias,
-      context: context,
-      builder: (BuildContext context) {
-        return SizedBox(
-          height: 300,
-          child: CustomScrollView(
-            controller: scrollController,
-            slivers: [
-              SliverAppBar(
-                backgroundColor: Colors.white,
-                surfaceTintColor: Colors.transparent,
-                leading: const SizedBox(),
-                actions: [
-                  IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-                title: Text(
-                  "${_isMonthly ? '월' : '연도'} 선택하기",
-                  style: const TextStyle(
-                    fontSize: Sizes.size16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                centerTitle: true,
-                pinned: true,
-              ),
-              SliverFixedExtentList(
-                itemExtent: itemExtent,
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    final int year = _isMonthly
-                        ? index < _now.month
-                            ? _now.year
-                            : _now.year - (index - _now.month) ~/ 12 - 1
-                        : _now.year - index;
-                    final int month =
-                        _isMonthly ? (_now.month - index - 1) % 12 + 1 : 1;
-
-                    return ListTile(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(Sizes.size8),
-                        ),
-                      ),
-                      tileColor: Colors.white,
-                      selected: index == selectedIndex,
-                      selectedTileColor:
-                          Theme.of(context).primaryColor.withOpacity(0.1),
-                      title: Text(
-                        _isMonthly ? '$year년 $month월' : '$year년',
-                        style: const TextStyle(fontSize: Sizes.size16),
-                      ),
-                      onTap: () {
-                        Navigator.of(context).pop(DateTime(year, month));
-                      },
-                    );
-                  },
-                  childCount: itemCount,
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-
-    if (picked != null) {
-      setState(
-        () {
-          if (_isMonthly) {
-            _monthlyYear = picked.year;
-            _monthlyMonth = picked.month;
-            _monthlyMoodEntries = generateMonthlyMoodEntries();
-          } else {
-            _yearlyYear = picked.year;
-            _yearlyMoodEntries = generateYearlyMoodEntries();
-            _modeYearlyMoodEntries = modeYearlyMoodEntries(
-              _yearlyMoodEntries,
-              _yearlyYear,
-            );
-          }
-        },
-      );
-    }
   }
 
   @override
@@ -197,59 +42,9 @@ class _DashboardScreenState extends State<DashboardScreen>
         ),
         body: TabBarView(
           controller: _tabController,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size20,
-              ),
-              child: Column(
-                children: [
-                  DateSelectorTab(
-                    text: "$_monthlyYear년 $_monthlyMonth월",
-                    onTap: _onDateSelectorTap,
-                  ),
-                  Gaps.v32,
-                  MoodChartCard(
-                    title: "기분 흐름",
-                    content: MoodFlowChart(
-                        moodEntries: _monthlyMoodEntries, isMonthly: true),
-                  ),
-                  Gaps.v32,
-                  MoodChartCard(
-                    title: "기분 분포",
-                    content: MoodDistChart(
-                      moodEntries: _monthlyMoodEntries,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size20,
-              ),
-              child: Column(
-                children: [
-                  DateSelectorTab(
-                    text: "$_yearlyYear년",
-                    onTap: _onDateSelectorTap,
-                  ),
-                  Gaps.v32,
-                  MoodChartCard(
-                    title: "기분 흐름",
-                    content: MoodFlowChart(
-                        moodEntries: _modeYearlyMoodEntries, isMonthly: false),
-                  ),
-                  Gaps.v32,
-                  MoodChartCard(
-                    title: "기분 분포",
-                    content: MoodDistChart(
-                      moodEntries: _yearlyMoodEntries,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          children: const [
+            MonthlyDashboard(),
+            YearlyDashboard(),
           ],
         ),
       ),
---
+++ lib/features/dashboard/widgets/show_date_selection_sheet.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:moodiary/constants/sizes.dart';
+
+Future<DateTime?> showDateSelectionSheet({
+  required BuildContext context,
+  required ScrollController scrollController,
+  required bool isMonthly,
+  required double itemExtent,
+  required int selectedIndex,
+  required int itemCount,
+  required DateTime now,
+}) {
+  return showModalBottomSheet<DateTime>(
+    clipBehavior: Clip.antiAlias,
+    context: context,
+    builder: (BuildContext context) {
+      return SizedBox(
+        height: 300,
+        child: CustomScrollView(
+          controller: scrollController,
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.transparent,
+              leading: const SizedBox(),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+              title: Text(
+                isMonthly ? '월 선택하기' : '연도 선택하기',
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              centerTitle: true,
+              pinned: true,
+            ),
+            SliverFixedExtentList(
+              itemExtent: itemExtent,
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  final int year = isMonthly
+                      ? index < now.month
+                          ? now.year
+                          : now.year - (index - now.month) ~/ 12 - 1
+                      : now.year - index;
+                  final int month =
+                      isMonthly ? (now.month - index - 1) % 12 + 1 : 1;
+
+                  return ListTile(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(Sizes.size8),
+                      ),
+                    ),
+                    tileColor: Colors.white,
+                    selected: index == selectedIndex,
+                    selectedTileColor:
+                        Theme.of(context).primaryColor.withOpacity(0.1),
+                    title: Text(
+                      isMonthly ? '$year년 $month월' : '$year년',
+                      style: const TextStyle(fontSize: Sizes.size16),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pop(DateTime(year, month));
+                    },
+                  );
+                },
+                childCount: itemCount,
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
