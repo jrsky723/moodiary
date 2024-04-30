a829814852ce510293bde45c4ad388c217e2ca34
ms <alstjq1012@gmail.com>
2024-04-30 22:03:40 +0900

    시간 tap 수정

--- lib/features/add_diary/widgets/sleep_time_picker.dart
+++ lib/features/add_diary/widgets/sleep_time_picker.dart
@@ -1,93 +1,207 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
 
 class SleepDialog extends StatefulWidget {
   final BuildContext context;
-  const SleepDialog({
-    super.key,
-    required this.context,
-  });
+  const SleepDialog({super.key, required this.context});
 
   @override
-  _SleepDialogState createState() => _SleepDialogState();
+  State<SleepDialog> createState() => _SleepDialogState();
 }
 
 class _SleepDialogState extends State<SleepDialog> {
-  DateTime? bedtime;
-  DateTime? wakeTime;
+  String? bPeriod = DateTime.now().hour >= 12 ? "PM" : "AM"; // AM or PM
+  int? bHour = DateTime.now().hour; // Hour of the day
+  int? bMinute = DateTime.now().minute; // Minute
 
-  void _openTimePicker() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("시간 선택"),
-          content: SizedBox(
-            height: 200, // 다이얼로그 크기 조절
-            child: Column(
-              children: [
-                ElevatedButton(
-                  child: const Text("취침 시각 선택"),
-                  onPressed: () => _pickTime(isBedTime: true),
-                ),
-                ElevatedButton(
-                  child: const Text("기상 시각 선택"),
-                  onPressed: () => _pickTime(isBedTime: false),
-                ),
-                if (bedtime != null)
-                  Text("취침 시각: ${bedtime!.hour}시${bedtime!.minute}분"),
-                if (wakeTime != null)
-                  Text("기상 시각: ${wakeTime!.hour}시${wakeTime!.minute}분"),
-              ],
-            ),
-          ),
-        );
-      },
-      barrierColor: Colors.black54, // 배경 어둡게 처리
-    );
+  String? wPeriod = DateTime.now().hour >= 12 ? "PM" : "AM"; // AM or PM
+  int? wHour = DateTime.now().hour; // Hour of the day
+  int? wMinute = DateTime.now().minute; // Minute
+
+  int? totalHour;
+  int? totalMinute;
+
+  void _getTotalHour(int? H1, int? H2) {
+    setState(() {
+      totalHour = H1! - H2!;
+    });
   }
 
-  void _pickTime({required bool isBedTime}) {
-    DatePicker.showTimePicker(
-      context,
-      showTitleActions: true,
-      onConfirm: (date) {
-        setState(() {
-          if (isBedTime) {
-            bedtime = date;
-          } else {
-            wakeTime = date;
-          }
-        });
+  void _getTotalMinute(int? M1, int? M2) {
+    setState(() {
+      totalMinute = M1! - M2!;
+    });
+  }
+
+  String formattedTime(String? period, int? hour, int? minute) =>
+      "${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')} $period";
+
+  void _showTimePickerDialog(
+      bool isBedtime, String title, String? period, int? hour, int? minute) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    DropdownButton<String>(
+                      value: period,
+                      items: ['AM', 'PM'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          period = newValue!;
+                        });
+                      },
+                    ),
+                    DropdownButton<int>(
+                      value: hour,
+                      items: List<int>.generate(12, (index) => index + 1)
+                          .map((int value) {
+                        return DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                        );
+                      }).toList(),
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          hour = newValue!;
+                        });
+                      },
+                    ),
+                    DropdownButton<int>(
+                      value: minute,
+                      items: List<int>.generate(60, (index) => index)
+                          .map((int value) {
+                        return DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString().padLeft(2, '0')),
+                        );
+                      }).toList(),
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          minute = newValue!;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  // Update the state of the parent widget when the dialog closes.
+                  _getTotalHour(bHour, wHour);
+                  _getTotalMinute(bMinute, wMinute);
+                  if (isBedtime) {
+                    bPeriod = period;
+                    bHour = hour;
+                    bMinute = minute;
+                  } else {
+                    wPeriod = period;
+                    wHour = hour;
+                    wMinute = minute;
+                  }
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('Set Time'),
+            ),
+          ],
+        );
       },
-      currentTime: DateTime.now(),
-      locale: LocaleType.en,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        elevation: 1,
-        backgroundColor: Colors.grey.shade300,
-        foregroundColor: Colors.grey.shade500,
-        surfaceTintColor: Colors.grey.shade100,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
+    return Column(
+      children: [
+        if (totalHour != null && totalMinute != null)
+          Text("취침 시간: $totalHour시간 $totalMinute분"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () => _showTimePickerDialog(
+                      true, "취침 시각", bPeriod, bHour, bMinute),
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.transparent,
+                    backgroundColor: Colors.grey.shade300,
+                    surfaceTintColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                  ),
+                  child: Text(
+                    formattedTime(
+                      bPeriod,
+                      bHour,
+                      bMinute,
+                    ),
+                    style: TextStyle(color: Colors.grey.shade800),
+                  ),
+                ),
+                Text(
+                  "취침",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () => _showTimePickerDialog(
+                      false, "기상 시각", wPeriod, wHour, wMinute),
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.transparent,
+                    backgroundColor: Colors.grey.shade300,
+                    surfaceTintColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                  ),
+                  child: Text(
+                    formattedTime(
+                      wPeriod,
+                      wHour,
+                      wMinute,
+                    ),
+                    style: TextStyle(color: Colors.grey.shade800),
+                  ),
+                ),
+                Text(
+                  "기상",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-      onPressed: _openTimePicker,
-      child: Flex(
-        direction: Axis.horizontal,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "수면을 기록해주세요",
-            style: TextStyle(color: Colors.grey.shade700),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
