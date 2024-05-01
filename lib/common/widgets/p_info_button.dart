a62a8eaa7061d7ddac067e3fbbf72af289e136fd
ms <alstjq1012@gmail.com>
2024-05-01 17:03:53 +0900

    AddDiary - SleepTimeTap
    수면 시간 계산하는 기능
    sleepTime tap UI수정
    AddDiary tap의 이름과 아이콘수정하는 SettingsScreen 화면 setup

--- lib/common/widgets/p_info_button.dart
+++ lib/common/widgets/p_info_button.dart
@@ -6,31 +6,25 @@ class pInfoButton extends StatelessWidget {
   final IconData icon;
   final double? size;
   final Color? color;
+  final Function()? onTap;
+
   const pInfoButton({
     super.key,
     required this.icon,
     this.size = Sizes.size14,
     this.color = Colors.grey,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: Sizes.size10,
+    return IconButton(
+      icon: FaIcon(
+        icon,
+        size: size,
+        color: color,
       ),
-      child: GestureDetector(
-        onTap: () {},
-        child: FaIcon(
-          icon,
-          size: size,
-          color: color,
-        ),
-      ),
+      onPressed: onTap,
     );
   }
 }
-
-// Vertical Divider 작동 안됨 있긴한데 안보임
-// gestureDetector 를 이용해서 클릭하면 reverse되어서 뒤에는 글이 앞에는 이모지 보이게 설정 
-//  + text overflow되면 아이콘 만들어서 펼치기 기능 가능하도록 설정(optional)
