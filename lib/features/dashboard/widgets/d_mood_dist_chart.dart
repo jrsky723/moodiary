c598207a84c0fd8759c18b015156a904fa6f5312
hanu607 <jrsky723@gmail.com>
2024-07-25 04:56:34 +0900

    Dashboard - 초기 세팅
    사용안할 위젯 표시 (d_: deleted)
    기본적인 dashboard layout 구성

---
+++ lib/features/dashboard/widgets/d_mood_dist_chart.dart
@@ -0,0 +1,88 @@
+// import 'package:flutter/material.dart';
+// import 'package:moodiary/constants/mood.dart';
+// import 'package:moodiary/constants/sizes.dart';
+// import 'package:moodiary/features/dashboard/models/mood_entry.dart';
+// import 'package:moodiary/features/dashboard/painters/bar_chart_painter.dart';
+// import 'package:moodiary/features/dashboard/painters/legend_painter.dart';
+// import 'package:moodiary/generated/l10n.dart';
+
+// class MoodDistChart extends StatelessWidget {
+//   final List<MoodEntry> moodEntries;
+//   const MoodDistChart({
+//     super.key,
+//     required this.moodEntries,
+//   });
+
+//   Mood getMostFrequentMood() {
+//     final Map<Mood, int> frequencyMap = {};
+
+//     for (var entry in moodEntries) {
+//       frequencyMap[entry.mood] = (frequencyMap[entry.mood] ?? 0) + 1;
+//     }
+
+//     Mood mostFrequentMood = Mood.values.first;
+//     int maxFrequency = 0;
+//     for (var mood in Mood.values) {
+//       final frequency = frequencyMap[mood] ?? 0;
+//       if (frequency > maxFrequency) {
+//         mostFrequentMood = mood;
+//         maxFrequency = frequency;
+//       }
+//     }
+
+//     return mostFrequentMood;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(
+//       builder: (context, constraints) {
+//         final height = constraints.maxHeight;
+//         final width = constraints.maxWidth;
+//         final legendWidth = width / 8;
+//         final barChartWidth = (width - legendWidth);
+//         final chartHeight = height * 0.9;
+//         final textHeight = height * 0.1;
+
+//         return Column(
+//           children: [
+//             Row(
+//               children: [
+//                 CustomPaint(
+//                   painter: LegendPainter(
+//                     colors: Mood.values.map((mood) => mood.color).toList(),
+//                   ),
+//                   child: SizedBox(
+//                     width: legendWidth,
+//                     height: chartHeight,
+//                   ),
+//                 ),
+//                 CustomPaint(
+//                   painter: BarChartPainter(
+//                     moodEntries: moodEntries,
+//                   ),
+//                   child: SizedBox(
+//                     width: barChartWidth,
+//                     height: chartHeight,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             SizedBox(
+//               height: textHeight,
+//               child: Text(
+//                 S
+//                     .of(context)
+//                     .mostFrequentMoodText(getMostFrequentMood().label(context)),
+//                 style: const TextStyle(
+//                   fontSize: Sizes.size12,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
