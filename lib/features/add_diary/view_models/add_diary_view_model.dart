2404af81446fb4428908af3c6626f8260340a714
ms <94189972+kaeba0616@users.noreply.github.com>
2024-08-16 13:34:14 +0900

    viewmodelprovider

--- lib/features/add_diary/view_models/add_diary_view_model.dart
+++ lib/features/add_diary/view_models/add_diary_view_model.dart
@@ -1,29 +1,38 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:moodiary/features/add_diary/models/add_diary_model.dart';
 import 'package:moodiary/features/add_diary/repos/add_diary_repo.dart';
 
-class AddDiaryViewModel extends ChangeNotifier {
+class AddDiaryViewModel extends Notifier<AddDiaryModel> {
   final AddDiaryRepository _repository;
-
-  late final AddDiaryModel _model = AddDiaryModel(
-    images: _repository.getImages(),
-    content: _repository.getContent(),
-  );
 
   AddDiaryViewModel(this._repository);
 
-  String get content => _model.content;
-  List<String> get images => _model.images;
-
   void setContent(String value) {
     _repository.setContent(value);
-    _model.content = value;
-    notifyListeners();
+    state = AddDiaryModel(
+      images: state.images,
+      content: value,
+    );
   }
 
   void setImages(List<String> value) {
     _repository.setImages(value);
-    _model.images = value;
-    notifyListeners();
+    state = AddDiaryModel(
+      images: value,
+      content: state.content,
+    );
+  }
+
+  @override
+  AddDiaryModel build() {
+    return AddDiaryModel(
+      images: _repository.getImages(),
+      content: _repository.getContent(),
+    );
   }
 }
+
+final addDiaryViewModelProvider =
+    NotifierProvider<AddDiaryViewModel, AddDiaryModel>(
+  () => throw UnimplementedError(),
+);
