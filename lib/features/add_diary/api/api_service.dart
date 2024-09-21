916bcb4b55b9467418f65ef8df5c48b081179c04
ms <94189972+kaeba0616@users.noreply.github.com>
2024-09-21 15:41:14 +0900

    add-diary firebase cluodstore에 더미데이터 post 테스트 완료

--- lib/features/add_diary/api/api_service.dart
+++ lib/features/add_diary/api/api_service.dart
@@ -1,78 +1,78 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
-import 'package:moodiary/features/add_diary/models/add_diary_model.dart';
+// import 'package:dio/dio.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:http_parser/http_parser.dart';
+// import 'package:mime/mime.dart';
+// import 'package:moodiary/features/add_diary/models/add_diary_model.dart';
 
-class ApiService {
-  final String x_api_key = dotenv.env['X_API_KEY']!;
-  final String baseURL = dotenv.env['BASE_URL']!;
-  final Dio dio = Dio();
+// class ApiService {
+//   final String x_api_key = dotenv.env['X_API_KEY']!;
+//   final String baseURL = dotenv.env['BASE_URL']!;
+//   final Dio dio = Dio();
 
-  void addJsonFieldToFormData(
-      FormData formData, String fieldName, String fieldValue) {
-    formData.files.add(
-      MapEntry(
-        fieldName,
-        MultipartFile.fromString(
-          fieldValue,
-          contentType: MediaType('application', 'json'),
-        ),
-      ),
-    );
-  }
+//   void addJsonFieldToFormData(
+//       FormData formData, String fieldName, String fieldValue) {
+//     formData.files.add(
+//       MapEntry(
+//         fieldName,
+//         MultipartFile.fromString(
+//           fieldValue,
+//           contentType: MediaType('application', 'json'),
+//         ),
+//       ),
+//     );
+//   }
 
-  Future<void> postDiary(AddDiaryModel model) async {
-    try {
-      final url = '$baseURL/add-diary';
-      final headers = {
-        "x-api-key": x_api_key,
-      };
-      final body = model.toJson();
+//   Future<void> postDiary(AddDiaryModel model) async {
+//     try {
+//       final url = '$baseURL/add-diary';
+//       final headers = {
+//         "x-api-key": x_api_key,
+//       };
+//       final body = model.toJson();
 
-      FormData formData = FormData();
+//       FormData formData = FormData();
 
-      addJsonFieldToFormData(
-          formData, 'author_id', body['author_id'].toString());
-      addJsonFieldToFormData(formData, 'content', body['content'].toString());
-      addJsonFieldToFormData(
-          formData, 'is_public', body['is_public'].toString());
+//       addJsonFieldToFormData(
+//           formData, 'author_id', body['author_id'].toString());
+//       addJsonFieldToFormData(formData, 'content', body['content'].toString());
+//       addJsonFieldToFormData(
+//           formData, 'is_public', body['is_public'].toString());
 
-      for (int i = 0; i < model.photos.length; i++) {
-        final filePath = model.photos[i].path;
+//       for (int i = 0; i < model.imageUrls.length; i++) {
+//         final filePath = model.imageUrls[i];
 
-        final mimeType = lookupMimeType(filePath);
-        final mimeTypeData = mimeType!.split('/');
+//         final mimeType = lookupMimeType(filePath);
+//         final mimeTypeData = mimeType!.split('/');
 
-        formData.files.add(
-          MapEntry(
-            'photos',
-            await MultipartFile.fromFile(
-              filePath,
-              contentType: MediaType(mimeTypeData[0], mimeTypeData[1]),
-            ),
-          ),
-        );
-      }
+//         formData.files.add(
+//           MapEntry(
+//             'photos',
+//             await MultipartFile.fromFile(
+//               filePath,
+//               contentType: MediaType(mimeTypeData[0], mimeTypeData[1]),
+//             ),
+//           ),
+//         );
+//       }
 
-      final response = await dio.post(
-        url,
-        data: formData,
-        options: Options(
-          headers: headers,
-        ),
-      );
+//       final response = await dio.post(
+//         url,
+//         data: formData,
+//         options: Options(
+//           headers: headers,
+//         ),
+//       );
 
-      if (response.statusCode == 200) {
-        log('Post success');
-      } else {
-        log('Error: ${response.statusCode}');
-        log('Response data: ${response.data}');
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-}
+//       if (response.statusCode == 200) {
+//         log('Post success');
+//       } else {
+//         log('Error: ${response.statusCode}');
+//         log('Response data: ${response.data}');
+//       }
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+// }
