c0439bc9ba368e2d0efb47a8836a195a4f55d7d8
jrsky <jrsky723@gmail.com>
2024-09-22 22:37:58 +0900

    Community - Community Post
    - Community Screen 기능, UI 수정
    - .env 파일 위치 변경
    - 더보기 기능 구현

---
+++ lib/common/widgets/image_network.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ImageNetwork extends StatelessWidget {
+  const ImageNetwork({
+    super.key,
+    required this.imageUrl,
+  });
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
+      frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+        if (wasSynchronouslyLoaded) {
+          return child;
+        }
+        return AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: frame == null ? 0 : 1,
+          child: child,
+        );
+      },
+      errorBuilder: (context, error, stackTrace) {
+        return const Icon(
+          Icons.error,
+          color: Colors.grey,
+        );
+      },
+    );
+  }
+}
