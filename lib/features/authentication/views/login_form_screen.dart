71f3f9aef376255f52108e5c2f3b94aecffee36c
ms <94189972+kaeba0616@users.noreply.github.com>
2024-09-25 15:17:06 +0900

    firebase authentication 기능 추가

---
+++ lib/features/authentication/views/login_form_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moodiary/constants/gaps.dart';
+import 'package:moodiary/constants/sizes.dart';
+import 'package:moodiary/features/authentication/view_models/login_view_model.dart';
+import 'package:moodiary/features/authentication/views/widgets/form_button.dart';
+import 'package:moodiary/generated/l10n.dart';
+
+class LoginFormScreen extends ConsumerStatefulWidget {
+  const LoginFormScreen({super.key});
+
+  @override
+  ConsumerState<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends ConsumerState<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Map<String, String> formData = {};
+
+  void _onSubmitTap() {
+    if (_formKey.currentState != null) {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        ref
+            .read(loginProvider.notifier)
+            .login(formData['email']!, formData['password']!, context);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).login),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size20,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['email'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['password'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: FormButton(
+                  disabled: false,
+                  text: S.of(context).login,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
