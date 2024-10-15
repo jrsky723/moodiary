73c0c35b6e69fe7ca93bb03948cb0e809dd215e9
ms <94189972+kaeba0616@users.noreply.github.com>
2024-10-15 22:50:41 +0900

    sign up시 userProfile 만드는 screen 생성

--- lib/features/authentication/view_models/signup_view_model.dart
+++ lib/features/authentication/view_models/signup_view_model.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:moodiary/features/authentication/repos/authentication_repo.dart';
+import 'package:moodiary/features/authentication/views/create_profile_screen.dart';
+import 'package:moodiary/features/users/view_models/user_profile_view_model.dart';
+import 'package:moodiary/utils/firebase_utils.dart';
 
 class SignUpViewModel extends AsyncNotifier<void> {
   late final AuthenticationRepository _authRepo;
@@ -11,14 +16,26 @@ class SignUpViewModel extends AsyncNotifier<void> {
     _authRepo = ref.read(authRepo);
   }
 
-  Future<void> signUp() async {
+  Future<void> signUp(BuildContext context) async {
     state = const AsyncValue.loading();
     final form = ref.read(signUpForm);
+    final user = ref.read(usersProvider.notifier);
     state = await AsyncValue.guard(
-      () async => await _authRepo.emailSignUp(
-        form["email"],
-        form["password"],
-      ),
+      () async {
+        final userCredential = await _authRepo.emailSignUp(
+          form["email"],
+          form["password"],
+        );
+        await user.createProfile(userCredential);
+      },
+    );
+    if (state.hasError) {
+      showFirebaseErrorSnack(context, state.error);
+    }
+    print('${form['username']}');
+    context.goNamed(
+      CreateProfileScreen.routeName,
+      extra: form['username'],
     );
   }
 }
